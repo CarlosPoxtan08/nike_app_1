5a894d9e3758b72cc623cdfd2d1dd019d3ee73eb
CarlosPoxtan08 <carlospoxtan08@gmail.com>
2025-03-21 00:55:17 -0600

    Segundo commit

--- lib/nike_shoes_store_app/main_nike_shoes_store_app.dart
+++ lib/nike_shoes_store_app/main_nike_shoes_store_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nike_app_1/nike_shoes_store_app/nike_shoes.dart';
 
 class MainNikeShoesStoreApp extends StatelessWidget {
   @override
@@ -19,13 +20,22 @@ class NikeShoesStoreHome extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Padding(
-            padding: const EdgeInsets.all(20.0),
+            padding: const EdgeInsets.only(left: 20.0, top: 20.0, right: 20.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
                 Image.asset(
                   'assets/nike_shoes_store/nike_logo.png',
                   height: 40,
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: shoes.length,
+                    itemBuilder: (context, index) {
+                      final shoesItem = shoes[index];
+                      return NikeShoesItem(shoesItem: shoesItem);
+                    },
+                  ),
                 ),
               ],
             ),
@@ -66,6 +76,86 @@ class NikeShoesStoreHome extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class NikeShoesItem extends StatelessWidget {
+  final NikeShoes shoesItem;
+
+  const NikeShoesItem({super.key, required this.shoesItem});
+
+  @override
+  Widget build(BuildContext context) {
+    const itemHeight = 290.0;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15.0),
+      child: SizedBox(
+        height: itemHeight,
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15.0),
+                  color: Color(shoesItem.color),
+                ),
+              ),
+            ),
+            Align(
+                alignment: Alignment.topCenter,
+                child: SizedBox(
+                  height: itemHeight * 0.6,
+                  child: FittedBox(
+                    child: Text(
+                      shoesItem.modelNumber.toString(),
+                      style: TextStyle(
+                        color: Colors.black12,
+                      ),
+                    ),
+                  ),
+                )),
+            Positioned(
+              top: 10,
+              left: 30,
+              right: 30,
+              height: itemHeight * 0.65,
+              child: Image.asset(
+                shoesItem.images.first,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Positioned(
+              bottom: 20,
+              left: 20,
+              child: Icon(
+                Icons.favorite_border,
+                color: Colors.grey,
+              ),
+            ),
+            Positioned(
+              bottom: 20,
+              right: 20,
+              child: Icon(
+                Icons.shopping_cart,
+                color: Colors.grey,
+              ),
+            ),
+            Positioned(
+                child: Column(
+              children: <Widget>[
+                Text(
+                  shoesItem.model,
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            )),
+          ],
+        ),
       ),
     );
   }
--- lib/nike_shoes_store_app/nike_shoes.dart
+++ lib/nike_shoes_store_app/nike_shoes.dart
@@ -5,12 +5,14 @@ class NikeShoes {
     required this.currentPrice,
     required this.images,
     required this.modelNumber,
+    required this.color,
   });
   final String model;
   final double oldPrice;
   final double currentPrice;
   final List<String> images;
   final int modelNumber;
+  final int color;
 }
 
 final shoes = <NikeShoes>[
@@ -19,10 +21,47 @@ final shoes = <NikeShoes>[
     currentPrice: 149,
     oldPrice: 299,
     images: [
-      'assets/nike_shoes_store/shoes1_1.png'
-          'assets/nike_shoes_store/shoes1_2.png'
-          'assets/nike_shoes_store/shoes1_3.png'
+      'assets/nike_shoes_store/shoes1_1.png',
+      'assets/nike_shoes_store/shoes1_2.png',
+      'assets/nike_shoes_store/shoes1_3.png',
     ],
     modelNumber: 90,
+    color: 0xFFF6F6F6,
   ),
+  NikeShoes(
+    model: 'AIR MAX 270 Gold',
+    currentPrice: 199,
+    oldPrice: 349,
+    images: [
+      'assets/nike_shoes_store/shoes2_1.png',
+      'assets/nike_shoes_store/shoes2_2.png',
+      'assets/nike_shoes_store/shoes2_3.png',
+    ],
+    modelNumber: 270,
+    color: 0xFFFCF5EB,
+  ),
+  NikeShoes(
+    model: 'AIR MAX 95 Red',
+    currentPrice: 299,
+    oldPrice: 399,
+    images: [
+      'assets/nike_shoes_store/shoes3_1.png',
+      'assets/nike_shoes_store/shoes3_2.png',
+      'assets/nike_shoes_store/shoes3_3.png',
+    ],
+    modelNumber: 95,
+    color: 0xFFFEEFEF,
+  ),
+  NikeShoes(
+    model: 'AIR MAX 98 FREE',
+    currentPrice: 149,
+    oldPrice: 299,
+    images: [
+      'assets/nike_shoes_store/shoes4_1.png',
+      'assets/nike_shoes_store/shoes4_2.png',
+      'assets/nike_shoes_store/shoes4_3.png',
+    ],
+    modelNumber: 98,
+    color: 0xFFEDF3FE,
+  )
 ];
